c5a9e7fffef8884424b5da52a549b807895cad9e
sadys castro <sadyscastro@gmail.com>
2023-05-04 23:45:35 -0600

    creando carpeta docs

--- src/products/pages/InicioPage.jsx
+++ src/products/pages/InicioPage.jsx
@@ -3,6 +3,8 @@ import imagen1 from'../../assets/imageCarruse1.jpg'
 import imagen2 from'../../assets/imagescarrusel.jpg'
 import imagen3 from'../../assets/imagesCarrusel2.png'
 import imagen4 from'../../assets/imagesCarrusel4.jpg'
+import imagen6 from'../../assets/imagesCarrusel6.jpg'
+
 
 export const InicioPage = () => {
   return (
@@ -12,6 +14,8 @@ export const InicioPage = () => {
         <div className='carruselImgCont'> <img src={imagen2} alt=""  className='carruselImg' /></div>
         <div className='carruselImgCont'> <img src={imagen3} alt=""  className='carruselImg' /></div>
         <div className='carruselImgCont'> <img src={imagen4} alt=""  className='carruselImg' /></div>
+        <div className='carruselImgCont'> <img src={imagen6} alt=""  className='carruselImg' /></div>
+
       </section>
 
       <CardPro />
